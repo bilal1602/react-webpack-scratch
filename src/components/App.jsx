74895c9c820250df2296b0dbe574ba2bee93e516
bilal1602 <bilal.sajjad786@gnail.com>
2021-07-01 19:28:22 +0500

    Feat: added jsx extensions.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
-const App = () => {return <div> <h1>  Hello world  </h1></div>}
+function App (){
+    return <div> <h1>  Hello world  </h1></div>
+}
 
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -10,13 +10,16 @@ module.exports = {
     module:{
         rules:[
             {
-                test: /\.js$/,
+                test: /\.jsx?$/,
                 exclude: /node_modules/,
                 use:{
                     loader: 'babel-loader'
                 }
             }
         ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
     },
     plugins: [
         new HtmlWebpackPlugin({
